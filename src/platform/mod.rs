37acd423373202967237774b567a6f6c07056b79
lvillis <56720445+lvillis@users.noreply.github.com>
2026-03-26 11:56:36 +0800

    chore(ci): update

--- src/platform/mod.rs
+++ src/platform/mod.rs
@@ -153,7 +153,7 @@ pub(crate) fn init_logging(v: u8) {
         let filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new(lvl));
         if let Err(e) = fmt::Subscriber::builder()
             .with_env_filter(filter)
-            .with_target(false)
+            .with_target(true)
             .without_time()
             .with_writer(io::stderr)
             .try_init()
